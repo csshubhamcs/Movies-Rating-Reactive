2cb7a13cd321499373d30f148b7295429e349ad8
ShubhamSingh <shubhamsinghhrx@gmail.com>
2024-03-11 11:58:05 +0530

    movies service initial commit
    Reason:
    
    Fixes:

---
+++ movies-service/src/main/java/com/moviesservices/moviesservice/controller/MoviesController.java
@@ -0,0 +1,16 @@
+package com.moviesservices.moviesservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/movies")
+@RequiredArgsConstructor
+public class MoviesController {
+
+
+
+
+
+}
---
+++ movies-service/src/main/java/com/moviesservices/moviesservice/model/Movie.java
@@ -0,0 +1,16 @@
+package com.moviesservices.moviesservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    private MovieInfo movieInfo;
+    private List<Review> reviewList;
+}
